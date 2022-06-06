e3aa8416b7923564301715313008b1640a73f6a0
Faizan Haider <hfaizan53@gmail.com>
2022-06-07 01:26:11 +0500

    Testing

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -35,6 +35,7 @@ class Home extends Common {
     })
 
     $(".next").click(function(){
+      debugger
       var _this = this
       $('#msform').parsley().whenValidate({
         group: 'block-' + CI.currentTab
